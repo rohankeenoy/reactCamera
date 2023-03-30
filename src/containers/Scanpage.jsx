9cd71012d017c8235795af00a3e5d85e8b549cca
rohankeenoy <103291728+rohankeenoy@users.noreply.github.com>
2023-03-30 02:00:34 -0500

    Remove unnessary files

---
+++ src/containers/Scanpage.jsx
@@ -0,0 +1,38 @@
+import React, {useRef, useEffect,useState} from 'react'
+
+const Scanpage = () => {
+    const videoRef = useRef(null);
+    const photoRef = useRef(null);
+    const [hasPhoto, setHasPhoto] = useState(false);
+    const getVideo = () =>{
+        navigator.mediaDevices.getUserMedia({video:{width: 600, height: 600}})
+        .then(stream => {
+            let video = videoRef.current;
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(err =>{
+            console.log(err);
+
+        })
+    }
+
+    useEffect(()=>{
+        getVideo();
+    },[videoRef])
+
+    return(
+        <div className="Scanpage">
+            <p>Scan page</p>
+        <div className = "camera">
+
+
+        </div>
+        <div className={'result'+ (hasPhoto ? 'hasPhoto': '')}>
+            <canvas ref={photoRef}></canvas>
+        </div>
+    
+        </div>
+    )
+}
+export default Scanpage
